2eea06cb8ea9c56aae11ca8350a53895ffbc9ead
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2022-12-12 13:35:03 +0530

    add new activities

--- app/src/main/java/com/example/dictionary_of_pali_term/MainActivity.java
+++ app/src/main/java/com/example/dictionary_of_pali_term/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void toLiveBu(View view){
         Intent intent = new Intent(this, LiveBuddhaActivity.class);
+        startActivity(intent);
+    }
+
+    public void toDeclomation(View view){
+        Intent intent = new Intent(this, Deklomation.class);
+        startActivity(intent);
+    }
+
+    public void toSuttas(View view){
+        Intent intent = new Intent(this, Suttas.class);
+        startActivity(intent);
+    }
+
+    public void toTermins(View view){
+        Intent intent = new Intent(this, Termins.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/dictionary_of_pali_term/Suttas.java
@@ -0,0 +1,14 @@
+package com.example.dictionary_of_pali_term;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suttas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suttas);
+    }
+}
