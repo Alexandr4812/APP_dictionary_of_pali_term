094c5f2df30253193492908e6997ba614964bf85
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2023-08-29 21:03:33 +0530

    added Nissaya

--- app/src/main/java/com/example/dictionary_of_pali_term/RulesSamaneraActivity.java
+++ app/src/main/java/com/example/dictionary_of_pali_term/RulesSamaneraActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class RulesSamaneraActivity extends AppCompatActivity {
+
+    private Button buttonHome;
+    private Button buttonLiveToRulesSamanera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class RulesSamaneraActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Убрать панель навигации (если нужно)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        this.buttonHome = findViewById(R.id.buttonRulesSamaneraHome);
+        this.buttonLiveToRulesSamanera = findViewById(R.id.buttonliveToRulesSamanera);
     }
 
     public void toRulesSekhiyaAct(View view){
@@ -49,5 +57,19 @@ public class RulesSamaneraActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RulesSamaneraPabbajjaActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void toTextNissaya(View view) {
+        ScrollView scrollText = findViewById(R.id.viewScrollRulesSamaneraNissaya);
+        scrollText.setVisibility(view.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public void tobackDhammapada(View view) {
+        ScrollView scrollTextNissaya = findViewById(R.id.viewScrollRulesSamaneraNissaya);
+        scrollTextNissaya.setVisibility(view.INVISIBLE);
+        buttonHome.setVisibility(View.INVISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.INVISIBLE);
     }
 }
