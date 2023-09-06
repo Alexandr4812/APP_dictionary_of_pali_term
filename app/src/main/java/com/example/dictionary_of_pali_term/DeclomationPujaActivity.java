48f4122dec81efa4b6c23980d63d18622c31d685
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2023-09-06 20:37:09 +0530

    added text vandana ambokote

--- app/src/main/java/com/example/dictionary_of_pali_term/DeclomationPujaActivity.java
+++ app/src/main/java/com/example/dictionary_of_pali_term/DeclomationPujaActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ScrollView;
 
 public class DeclomationPujaActivity extends AppCompatActivity {
+
+    private Button buttonHome;
+    private Button buttonLiveToPuja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,14 @@ public class DeclomationPujaActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Убрать панель навигации (если нужно)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        this.buttonHome = findViewById(R.id.buttonPujaHome);
+        this.buttonLiveToPuja = findViewById(R.id.buttonliveToPuja);
     }
 
     public void toDeclomation(View view){
+        buttonHome.setVisibility(View.INVISIBLE);
+        buttonLiveToPuja.setVisibility(View.INVISIBLE);
         Intent intent = new Intent(this, DeklomationMainActivity.class);
         startActivity(intent);
         finish();
@@ -30,18 +39,33 @@ public class DeclomationPujaActivity extends AppCompatActivity {
     public void toDeclomaciyaMorningPuja(View view) {
         ScrollView scrollText = findViewById(R.id.overScrollTextMorningPuja);
         scrollText.setVisibility(view.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToPuja.setVisibility(View.VISIBLE);
+    }
+
+    public void toDeclomaciyaVandanaAmbokote(View view) {
+        ScrollView scrollText = findViewById(R.id.overScrollTextVandanaAmbokote);
+        scrollText.setVisibility(view.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToPuja.setVisibility(View.VISIBLE);
     }
 
     public void toDeclomaciyaVandanaWithLayman(View view) {
         ScrollView scrollText = findViewById(R.id.overScrollTextVandanaWithLayman);
         scrollText.setVisibility(view.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToPuja.setVisibility(View.VISIBLE);
     }
 
     public void tobackOver(View view) {
         ScrollView scrollText = findViewById(R.id.overScrollTextMorningPuja);
         ScrollView scrollTextVandanaWithLayman = findViewById(R.id.overScrollTextVandanaWithLayman);
+        ScrollView scrollTextVandanaAmbokote = findViewById(R.id.overScrollTextVandanaAmbokote);
         scrollText.setVisibility(view.INVISIBLE);
         scrollTextVandanaWithLayman.setVisibility(view.INVISIBLE);
+        scrollTextVandanaAmbokote.setVisibility(View.INVISIBLE);
+        buttonHome.setVisibility(View.INVISIBLE);
+        buttonLiveToPuja.setVisibility(View.INVISIBLE);
     }
 
     public void toMainAct(View view){
