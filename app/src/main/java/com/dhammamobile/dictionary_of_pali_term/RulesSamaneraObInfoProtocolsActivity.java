fbf06640f08a5b6bc899a24396696b62bd950155
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2023-11-04 16:29:38 +0530

    added next 3 parts of protocols texts.

--- app/src/main/java/com/dhammamobile/dictionary_of_pali_term/RulesSamaneraObInfoProtocolsActivity.java
+++ app/src/main/java/com/dhammamobile/dictionary_of_pali_term/RulesSamaneraObInfoProtocolsActivity.java
@@ -11,7 +11,8 @@ public class RulesSamaneraObInfoProtocolsActivity extends BaseActivityClass {
 
     private Button buttonHome, buttonLiveToRulesSamanera;
 
-    private ScrollView scrollTextProtocols, scrollTextProtocols2;
+    private ScrollView scrollTextProtocols, scrollTextProtocols2, scrollTextProtocols3,
+            scrollTextProtocols4, scrollTextProtocols5;
 
 
     @Override
@@ -27,6 +28,9 @@ public class RulesSamaneraObInfoProtocolsActivity extends BaseActivityClass {
 
         this.scrollTextProtocols = findViewById(R.id.viewScrollTextRulesSamaneraObInfoProtocols);
         this.scrollTextProtocols2 = findViewById(R.id.viewScrollTextRulesSamaneraObInfoProtocols2);
+        this.scrollTextProtocols3 = findViewById(R.id.viewScrollTextRulesSamaneraObInfoProtocols3);
+        this.scrollTextProtocols4 = findViewById(R.id.viewScrollTextRulesSamaneraObInfoProtocols4);
+        this.scrollTextProtocols5 = findViewById(R.id.viewScrollTextRulesSamaneraObInfoProtocols5);
     }
 
     public void toMainAct(View view){
@@ -55,9 +59,30 @@ public class RulesSamaneraObInfoProtocolsActivity extends BaseActivityClass {
         buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
     }
 
+    public  void toText3(View view) {
+        scrollTextProtocols3.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public  void toText4(View view) {
+        scrollTextProtocols4.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public  void toText5(View view) {
+        scrollTextProtocols5.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
     public void tobackRulesSamaneraObInfoProtocolAct(View view) {
         scrollTextProtocols.setVisibility(View.INVISIBLE);
         scrollTextProtocols2.setVisibility(View.INVISIBLE);
+        scrollTextProtocols3.setVisibility(View.INVISIBLE);
+        scrollTextProtocols4.setVisibility(View.INVISIBLE);
+        scrollTextProtocols5.setVisibility(View.INVISIBLE);
         buttonHome.setVisibility(View.INVISIBLE);
         buttonLiveToRulesSamanera.setVisibility(View.INVISIBLE);
     }
