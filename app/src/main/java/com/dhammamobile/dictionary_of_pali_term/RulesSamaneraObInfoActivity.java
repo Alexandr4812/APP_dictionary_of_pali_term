47ce7b0312f8c299de632f7fed2508e0dd614392
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2023-11-10 17:06:32 +0530

    added buttons and inform`s texts in protocols chapters. Protocol chapter all ready.

--- app/src/main/java/com/dhammamobile/dictionary_of_pali_term/RulesSamaneraObInfoActivity.java
+++ app/src/main/java/com/dhammamobile/dictionary_of_pali_term/RulesSamaneraObInfoActivity.java
@@ -10,7 +10,9 @@ public class RulesSamaneraObInfoActivity extends BaseActivityClass {
     private Button buttonLiveToRulesSamanera;
     private ScrollView scrollTextMajorInfo, scrollTextRulesSamaneraObInfoPermissible,
             scrollTextRulesSamaneraPurMoral, scrollTextRulesSamaneraUseMonksThings,
-            scrollTextRulesSamaneraDirectionMind, scrollTextRulesSamaneraTypesGoods;
+            scrollTextRulesSamaneraDirectionMind, scrollTextRulesSamaneraTypesGoods,
+            scrollTextRulesSamaneraGarmony, scrollTextRulesSamaneraCriticism,
+            scrollTextRulesSamaneraProtector, scrollTextRulesSamaneraTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,10 @@ public class RulesSamaneraObInfoActivity extends BaseActivityClass {
         this.scrollTextRulesSamaneraUseMonksThings = findViewById(R.id.viewScrollRulesSamaneraUseMonksThings);
         this.scrollTextRulesSamaneraDirectionMind = findViewById(R.id.viewScrollRulesSamaneraDirectionMind);
         this.scrollTextRulesSamaneraTypesGoods =findViewById(R.id.viewScrollRulesSamaneraTypesGoods);
+        this.scrollTextRulesSamaneraGarmony = findViewById(R.id.viewScrollRulesSamaneraGarmony);
+        this.scrollTextRulesSamaneraCriticism = findViewById(R.id.viewScrollRulesSamaneraCriticism);
+        this.scrollTextRulesSamaneraProtector = findViewById(R.id.viewScrollRulesSamaneraProtector);
+        this.scrollTextRulesSamaneraTo = findViewById(R.id.viewScrollRulesSamaneraTo);
     }
 
     public void toRulesSamaneraAct(View view){
@@ -85,6 +91,30 @@ public class RulesSamaneraObInfoActivity extends BaseActivityClass {
         buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
     }
 
+    public void toTextRulesSamaneraGarmony(View view) {
+        scrollTextRulesSamaneraGarmony.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public void toTextRulesSamaneraCriticism(View view) {
+        scrollTextRulesSamaneraCriticism.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public void toTextRulesSamaneraProtector(View view) {
+        scrollTextRulesSamaneraProtector.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
+    public void toTextRulesSamaneraTo(View view) {
+        scrollTextRulesSamaneraTo.setVisibility(View.VISIBLE);
+        buttonHome.setVisibility(View.VISIBLE);
+        buttonLiveToRulesSamanera.setVisibility(View.VISIBLE);
+    }
+
 
     public void tobackRulesSamaneraProtocolAct(View view) {
         scrollTextMajorInfo.setVisibility(View.INVISIBLE);
@@ -93,6 +123,10 @@ public class RulesSamaneraObInfoActivity extends BaseActivityClass {
         scrollTextRulesSamaneraUseMonksThings.setVisibility(View.INVISIBLE);
         scrollTextRulesSamaneraDirectionMind.setVisibility(View.INVISIBLE);
         scrollTextRulesSamaneraTypesGoods.setVisibility(View.INVISIBLE);
+        scrollTextRulesSamaneraGarmony.setVisibility(View.INVISIBLE);
+        scrollTextRulesSamaneraCriticism.setVisibility(View.INVISIBLE);
+        scrollTextRulesSamaneraProtector.setVisibility(View.INVISIBLE);
+        scrollTextRulesSamaneraTo.setVisibility(View.INVISIBLE);
         buttonHome.setVisibility(View.INVISIBLE);
         buttonLiveToRulesSamanera.setVisibility(View.INVISIBLE);
     }
