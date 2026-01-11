f6d89ae227e5c36baa5a2888351d7acca1e4e95a
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2026-01-11 15:26:41 +0530

    В раздел "Декламации" добывленны строфы 4 защитных медитаций.

--- app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Declomation/DeclomationOverActivity.java
+++ app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Declomation/DeclomationOverActivity.java
@@ -3,7 +3,10 @@ package com.dhammamobile.dictionary_of_pali_term.Declomation;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import androidx.core.graphics.Insets;
@@ -14,11 +17,17 @@ import com.dhammamobile.dictionary_of_pali_term.BaseActivityClass;
 import com.dhammamobile.dictionary_of_pali_term.MainActivity;
 import com.dhammamobile.dictionary_of_pali_term.R;
 
+import java.util.Locale;
+
 
 public class DeclomationOverActivity extends BaseActivityClass {
 
     private Button buttonHome;
     private Button buttonLiveToOver;
+
+    LinearLayout buttonOver;
+    Button plusText, minusText;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,51 @@ public class DeclomationOverActivity extends BaseActivityClass {
 
         this.buttonHome = findViewById(R.id.buttonDeclomationHome);
         this.buttonLiveToOver = findViewById(R.id.buttonliveToOver);
+
+        plusText = findViewById(R.id.buttonPlusOver);
+        minusText = findViewById(R.id.buttonMinusOver);
+
+        webView = findViewById(R.id.webViewOver);
+
+        buttonOver = findViewById(R.id.button_layout_over);
+
+        // Настройки WebView
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.clearCache(true);
+
+        // Обработчики нажатий кнопок для увеличения/уменьшения шрифта
+        plusText.setOnClickListener(v -> {
+            webView.evaluateJavascript("javascript:increaseFontSize();", null);
+        });
+
+        minusText.setOnClickListener(v -> {
+            webView.evaluateJavascript("javascript:decreaseFontSize();", null);
+        });
+    }
+
+    public void toDeclomaciyaChaturarakha(View view) {
+        buttonOver.setVisibility(View.VISIBLE);
+        webView.setVisibility(View.VISIBLE);
+        String htmlFilePath;
+        String currentLanguage = Locale.getDefault().getLanguage();
+        if (currentLanguage.equals("ru")) {
+            htmlFilePath = "file:///android_asset/parittas_ru/chaturarakha_ru.html";
+        } else {
+            htmlFilePath = "file:///android_asset/parittas_en/chaturarakha_en.html";
+        }
+        loadHtmlPage(htmlFilePath);
+    }
+
+    private void loadHtmlPage(String htmlFilePath) {
+        webView.loadUrl(htmlFilePath);
+    }
+
+
+
+    public void toOverBack(View view){
+        webView.setVisibility(View.INVISIBLE);
+        buttonOver.setVisibility(View.INVISIBLE);
     }
 
     public void toDeclomation(View view){
