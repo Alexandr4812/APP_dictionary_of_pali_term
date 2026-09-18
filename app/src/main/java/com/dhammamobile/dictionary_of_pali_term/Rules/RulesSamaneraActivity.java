0fe0d4deb3f469e1d8df0bcff7f31af990bc4065
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2026-09-18 10:31:17 +0530

    Обновлен раздел "руководство для саманер" правила Секхии.

--- app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Rules/RulesSamaneraActivity.java
+++ app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Rules/RulesSamaneraActivity.java
@@ -1,9 +1,13 @@
 package com.dhammamobile.dictionary_of_pali_term.Rules;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import androidx.activity.OnBackPressedCallback;
@@ -15,16 +19,20 @@ import com.dhammamobile.dictionary_of_pali_term.BaseActivityClass;
 import com.dhammamobile.dictionary_of_pali_term.MainActivity;
 import com.dhammamobile.dictionary_of_pali_term.R;
 
+import java.util.Locale;
+
 
 public class RulesSamaneraActivity extends BaseActivityClass {
 
+    LinearLayout buttonSekhiya;
     private Button buttonHome;
     private Button buttonLiveToRulesSamanera;
 
-
+    WebView webView;
 
     private ScrollView scrollTextNissaya;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,10 +50,17 @@ public class RulesSamaneraActivity extends BaseActivityClass {
             return insets;
         });
 
+        webView = findViewById(R.id.webViewSekhiya);
+        buttonSekhiya = findViewById(R.id.button_layout_sekhiya);
+
         this.buttonHome = findViewById(R.id.buttonRulesSamaneraHome);
         this.buttonLiveToRulesSamanera = findViewById(R.id.buttonliveToRulesSamanera);
         this.scrollTextNissaya = findViewById(R.id.viewScrollRulesSamaneraNissaya);
 
+        // Настройки WebView
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.clearCache(true);
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
@@ -53,10 +68,29 @@ public class RulesSamaneraActivity extends BaseActivityClass {
                 startIntentActivityAndFinish(RulesActivity.class);
             }
         });
+
+    }
+
+    private void loadHtmlPage(String htmlFilePath) {
+        webView.loadUrl(htmlFilePath);
     }
 
     public void toRulesSekhiyaAct(View view){
-        startIntentActivityAndFinish(RulesSamaneraSekhiyaActivity.class);
+        buttonSekhiya.setVisibility(View.VISIBLE);
+        webView.setVisibility(View.VISIBLE);
+        String htmlFilePath;
+        String currentLanguage = Locale.getDefault().getLanguage();
+        if (currentLanguage.equals("ru")) {
+            htmlFilePath = "file:///android_asset/samanera_rules_ru/sekhiya_ru.html";
+        } else {
+            htmlFilePath = "file:///android_asset/samanera_rules_ru/sekhiya_ru.html";
+        }
+        loadHtmlPage(htmlFilePath);
+    }
+
+    public void toSekhiyaBack(View view){
+        webView.setVisibility(View.INVISIBLE);
+        buttonSekhiya.setVisibility(View.INVISIBLE);
     }
 
     public void toRulesAct(View view){
