8f3bc79179ee59dfbddb27d29abe08c1cb3df8ef
Alexandr4812 <87494146+Alexandr4812@users.noreply.github.com>
2026-09-20 19:22:36 +0530

    Добавлен раздел с полной Сутта Питакой от open tipitaka на русском языке. Добавлен  поиск на русском языке по всем суттам этого раздела. В дальнейшем будут добавлены все комментарии, Винайя Питака и Абхидхамма Питака.

--- app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Suttas/SuttasOpenActivity.java
+++ app/src/main/java/com/dhammamobile/dictionary_of_pali_term/Suttas/SuttasOpenActivity.java
@@ -52,7 +52,7 @@ public class SuttasOpenActivity extends BaseActivityClass {
 
     private static final String DB_NAME = "suttapitaka_ru.db";
     private static final String ASSET_DB_PATH = "databases/" + DB_NAME;
-    private static final String ASSET_DB_VERSION = "3.5";
+    private static final String ASSET_DB_VERSION = "3.7";
 
     private static final String PREFS_NAME = "sutta_db_prefs";
     private static final String KEY_COPIED_DB_VERSION = "copied_db_version";
